acd58a3f172219901662c5601226e29773e14bda
João Vitor Assumpção <jrvitor5@gmail.com>
2019-09-27 15:08:58 -0300

    Propagando props para componentes

--- src/Componentes/Family.jsx
+++ src/Componentes/Family.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default props =>
     <div>
         <h1>Familia</h1>
-        {React.cloneElement(props.children, 
-            {sobrenome: props.sobrenome})}
+        {React.cloneElement(props.children,{sobrenome: props.sobrenome})}
+        
+        {/* {React.cloneElement(props.children, 
+            {sobrenome: props.sobrenome})} */}
+        
         {/* {props.children} */}
     </div>
